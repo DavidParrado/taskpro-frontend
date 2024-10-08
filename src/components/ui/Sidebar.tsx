a6df08d097ba1a9fb78a5ececd240712e5c406d9
DavidParrado <juandparrado04@gmail.com>
2024-10-07 23:49:11 -0500

    Añadiendo estilos al sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,31 +1,29 @@
 'use client';
 // components/Sidebar.tsx
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Sidebar = () => {
+
+  const routes = [
+    { name: 'Proyectos', path: '/projects' },
+    { name: 'Tareas', path: '/tasks' },
+    { name: 'Equipos', path: '/teams' },
+    { name: 'Perfil', path: '/profile' }
+  ];
+  const pathName = usePathname();
+  const isActive = (path: string) => pathName.startsWith(path) ? 'bg-indigo-100 dark:bg-gray-800' : '';
+
   return (
     <div className="w-64 bg-white dark:bg-trueGray-900 dark:text-slate-300 text-black py-5 px-4 lg:px-6 xl:px-8 border-r dark:border-r-slate-500">
       <ul className="list-none p-0">
-        <li className="mb-4">
-          <Link href="/projects" className="dark:text-slate-300 text-black text-lg hover:underline">
-            Proyectos
-          </Link>
-        </li>
-        <li className="mb-4">
-          <Link href="/tasks" className="dark:text-slate-300 text-black text-lg hover:underline">
-            Tareas
-          </Link>
-        </li>
-        <li className="mb-4">
-          <Link href="/teams" className="dark:text-slate-300 text-black text-lg hover:underline">
-            Equipos
-          </Link>
-        </li>
-        <li className="mb-4">
-          <Link href="/profile" className="dark:text-slate-300 text-black text-lg hover:underline">
-            Perfil
-          </Link>
-        </li>
+        {routes.map((route) => (
+          <li key={route.name} className="mb-4">
+            <Link href={route.path} className={`inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 ${isActive(route.path)}`}>
+              {route.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
